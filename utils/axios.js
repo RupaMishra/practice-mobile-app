9f8d1e57d95d616461ad8ccf3edb63fbb1e752e4
dshot7211 <dshot7211@gmail.com>
2024-03-19 12:02:38 +0530

    axios setup

--- utils/axios.js
+++ utils/axios.js
@@ -1,16 +1,30 @@
 import axios from "axios";
 import { getTokenFromLocalStorage } from "./localStorage";
 import { BASE_URL } from "../network/ApiEndpoints";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const customFetch = axios.create({
   baseURL: BASE_URL,
 });
 
-customFetch.interceptors.request.use(async (config) => {
-  config.headers["Companyid"] = `9`;
-  const token = await getTokenFromLocalStorage();
-  if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`;
+// customFetch.interceptors.request.use(async (config) => {
+//   config.headers["Companyid"] = `9`;
+//   const token = await getTokenFromLocalStorage();
+//   if (token) {
+//     config.headers["Authorization"] = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+customFetch.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = "Bearer " + token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-});
+);
