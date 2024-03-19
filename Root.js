2f67cb2522a83f59c93065c2c8680fb1043ed081
RupaMishra <rupamishraa2000@gmail.com>
2024-03-19 12:03:07 +0530

    logout

--- Root.js
+++ Root.js
@@ -2,7 +2,11 @@ import { useSelector } from "react-redux";
 import { AppNavigation } from "./navigations/AppNavigation";
 
 const Root = () => {
-  const { isAuthenticated } = useSelector((store) => store.auth);
+  const { isAuthenticated, user } = useSelector((store) => store.auth);
+
+  console.log("====================================");
+  console.log(user);
+  console.log("====================================");
 
   const returnNavigation = AppNavigation(isAuthenticated);
 
